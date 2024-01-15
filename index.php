f537f810ffce525f7ffed5f70ae90e40bc37b773
DaviGel <geloso.davide@gmail.com>
2024-01-15 16:35:18 +0100

    feat: bonus 1

--- index.php
+++ index.php
@@ -5,14 +5,25 @@ class Movie
   public $title;
   public $regia;
   public $year;
+  public $genres = [];
 
-  function __construct($title)
+  function __construct($_title)
   {
-    $this->title = $title;
+    $this->title = $_title;
   }
 
   public function getMovieInfo() {
-    echo "Titolo del film: $this->title<br>Regia: $this->regia<br>Anno:$this->year<br>";
+    $genres = $this->getGenreInfo($this->genres);
+    echo "Titolo del film: $this->title<br>Regia: $this->regia<br>Anno: $this->year<br>Generi: $genres<br><br>";
+  }
+
+  public function getGenreInfo($_genres) {
+    $result = '';
+    foreach ($_genres as $index=>$genre) {
+      
+      $result = $index === 0 ? $result .= $genre : $result .= " - $genre";
+    }
+    return $result;
   }
 }
 
@@ -20,11 +31,16 @@ $titanic = new Movie("Titanic");
 
 $titanic->regia = "James Cameron";
 $titanic->year = 1997;
+$titanic->genres[] = "Romantico";
+$titanic->genres[] = "Disastro";
 
 $avatar = new Movie("Avatar");
 
 $avatar->regia = "James Cameron";
 $avatar->year = 2009;
+$avatar->genres[] = "Azione";
+$avatar->genres[] = "Avventura";
+$avatar->genres[] = "Fantastico";
 
 $titanic->getMovieInfo();
 $avatar->getMovieInfo();
